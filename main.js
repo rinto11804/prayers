5e7fd19785b7c758580e5c52f3a2b87b6e3c1070
rinto11804 <rinto11804@gmail.com>
2021-10-24 19:39:23 +0530

    merged all components to main.js

--- main.js
+++ main.js
@@ -1,14 +1,54 @@
 const app = Vue.createApp({
     data() {
         return {
-            count: 1
+
+        }
+    }
+});
+const CardCounterButton = {
+    template: `
+    <button class="counter" ref="counter" @click="Incrementer">{{count}}</button>
+    <slot></slot>
+    `,
+    data() {
+        return {
+            count: 0,
+            finished: false
+        }
+    },
+    methods: {
+        Incrementer() {
+            if (this.count < 10)
+                this.count += 1
+        }
+    }
+}
+app.component('Card', {
+    props: {
+
+    },
+    components: {CardCounterButton},
+    template: `
+    <div v-for="happy in happys" class="card">
+        <h3>{{happy.cardNum}}</h3>
+        <h2>{{happy.cardTitle}}</h2>
+        <img :src="happy.imageUrl">
+        <CardCounterButton>Nanma</CardCounterButton>
+    </div>
+    `,
+    data() {
+        return {
+            happys: null
         }
     },
     methods: {},
-    computed: {}
+    computed: {},
+    mounted() {
+        fetch('https://demo1api.herokuapp.com/api/happy')
+            .then(res => res.json())
+            .then(data => this.happys = data)
+
+    }
 })
 
 
-//fetch('https://demo1api.herokuapp.com/')
-//    .then(res => res.json())
-//    .then(data => console.log(data))
